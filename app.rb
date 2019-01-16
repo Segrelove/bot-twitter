25b6b48e68f754f0bbde286c030dcb5858d58d5f
Segrelove <mlesegret@gmail.com>
2019-01-16 14:46:49 +0100

    Exercice bot twitter fonctionnel !

--- app.rb
+++ app.rb
@@ -33,7 +33,29 @@ end
 
 def request_hashtag
     @client.search('#bonjour_monde').take(25).each do |tweet|
-        puts tweet.inspect
+        @client.fav(tweet)
     end
 end
 
+def follow_hashtag
+    @client.search('#bonjour_monde').take(1).each do |tweet|
+        @client.follow tweet.user.id
+    end 
+end
+
+streaming = Twitter::Streaming::Client.new do |config|
+    config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+    config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+    config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+    config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+end
+
+@streaming = streaming
+
+def stream
+    @streaming.filter(track:'#bonjour_monde') do |tweet|
+    puts tweet.text
+        @client.fav(tweet)
+        @client.follow tweet.user.id
+    end
+end
